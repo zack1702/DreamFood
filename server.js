53631c00ea026766bc955a08ab21303f5ea18e19
elbayed zakaria <elbayed.zakaria2@gmail.com>
2021-10-28 22:51:47 +0100

    last essai

--- server.js
+++ server.js
@@ -25,6 +25,20 @@ app.use(cors());
 //app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({ origin: "http://localhost:3000/", credentials: true }));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+ });
+
 
 //depolyement
  if(process.env.NODE_ENV==="production"){
