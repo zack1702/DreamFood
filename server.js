569d5012a8e6689f2efcca3b7079229f0c2f0e3d
elbayed zakaria <elbayed.zakaria2@gmail.com>
2021-10-28 23:01:47 +0100

    last essai

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
     res.header(
        'Access-Control-Allow-Headers',
-       'Origin, X-Requested-With, Content-Type, Accept'
+       'Origin, X-Requested-With, Content-Type:application/json, Accept'
     );
     next();
  });
